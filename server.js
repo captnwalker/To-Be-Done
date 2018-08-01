a027fe1a3001da929d00c4d6b18fed880de802f1
Steve Walker <captnwalker@gmail.com>
2018-08-01 18:54:41 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+//  Define and map the partials
+hbs.registerPartials(__dirname + '/views/partials')
+
+app.set('view engine', 'hbs');
+
+
+
+// MIDDLEWARE
+// Generates date for server.log file
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  // Generate server.log file and appends changes
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log')
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+// Sets absolute path to public directory
+app.use(express.static(__dirname + '/public', ));
+
+// Handlebars Helpers
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+// Routes
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Fuck You Asshole!'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    name: '404',
+    errors: [
+      'You Suck',
+      'ID10t'
+    ]
+  });
+});
+
+// Start server on port 3000
+app.listen(3000, () => {
+  console.log('Nacelles have spun up on port 3000, Captain.')
+});
